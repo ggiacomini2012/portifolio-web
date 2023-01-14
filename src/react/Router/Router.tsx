272b63268bd64df19aea39f805454cbb25cca989
pro gui <ggiacomini2012@gmail.com>
2023-01-13 21:10:44 -0300

    refactor(router): delete ReduxSibling page and files

--- src/react/Router/Router.tsx
+++ src/react/Router/Router.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/about/About';
 import Home from './pages/home/Home';
 import ReduxPlayground from './pages/reduxPlayground/ReduxPlayground';
-import ReduxSibling from './pages/reduxSibling/ReduxSibling';
 
 function Router() {
   return (
@@ -12,7 +11,6 @@ function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/redux-playground" element={<ReduxPlayground />} />
-        <Route path="/redux-sibling" element={<ReduxSibling />} />
       </Routes>
     </BrowserRouter>
   );
