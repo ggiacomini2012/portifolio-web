29ec55ddcf2dcb3194fd258fcd6c655622d61598
pro gui <ggiacomini2012@gmail.com>
2023-01-20 19:06:20 -0300

    refactor(globalstrings): add new strings and interfaces

--- src/react/global/constants/strings/globalStrings.ts
+++ src/react/global/constants/strings/globalStrings.ts
@@ -1,7 +1,23 @@
-import IStrings from './strings.interface';
+import * as Interface from './strings.interface';
 
-const strings: IStrings = {
-  developerName: 'Guilherme Giacomini Teixeira',
+const globalStrings: Interface.IAllStrings = {
+  text: {
+    intro: 'intro',
+    by: 'by',
+  },
+  developerName: 'Guilherme Giacomini Teixeira.',
+  english: {
+    intro: 'Hi, I\'m Guilherme Giacomini Teixeira, full stack web developer.',
+    by: 'by',
+  },
+  portuguese: {
+    intro: 'Olá, Guilherme Giacomini Teixeira, desenvolvedor web full stack.',
+    by: 'por',
+  },
+  colorTheme: {
+    darkMode: '-dark',
+    lightMode: '-light',
+  },
 };
 
-export default strings;
+export default globalStrings;
--- src/react/global/constants/strings/strings.interface.ts
+++ src/react/global/constants/strings/strings.interface.ts
@@ -1,5 +1,29 @@
-interface IStrings {
-  developerName: string;
+interface ITexts {
+  intro: string;
+  by: string;
 }
 
-export default IStrings;
+interface IAllLanguages {
+  text: ITexts;
+  english: ITexts;
+  portuguese: ITexts;
+}
+
+interface IAllColorThemes {
+  darkMode: string;
+  lightMode: string;
+}
+
+interface IColorTheme {
+  colorTheme: IAllColorThemes
+}
+
+interface IDeveloper {
+  developerName: string
+}
+
+interface IAllStrings extends IColorTheme, IAllLanguages, IDeveloper {}
+
+export type {
+  IAllLanguages, ITexts, IAllColorThemes, IAllStrings, IColorTheme,
+};
