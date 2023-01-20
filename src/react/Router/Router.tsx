e4a1b4e923c18aed81806273f9eb7d819cef3cfd
pro gui <ggiacomini2012@gmail.com>
2023-01-20 19:10:27 -0300

    refactor(router): add theme to router and import Router.css

--- src/react/Router/Router.tsx
+++ src/react/Router/Router.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import functions from '../global/functions';
+import { useColorTheme } from '../state-manager/redux/slices/sliceColorTheme';
 import About from './pages/about/About';
 import Home from './pages/home/Home';
 import ReduxPlayground from './pages/reduxPlayground/ReduxPlayground';
+import './Router.css';
 
 function Router() {
+  const [themeState] = useSelector(useColorTheme);
+
+  const theme = functions.colorThemeSelector(themeState.colorTheme);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/redux-playground" element={<ReduxPlayground />} />
-      </Routes>
-    </BrowserRouter>
+    <div className={`router${theme}`}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/redux-playground" element={<ReduxPlayground />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
