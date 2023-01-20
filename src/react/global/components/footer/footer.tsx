2afef9c30d4769d60ee3da2a7f915602d085d408
pro gui <ggiacomini2012@gmail.com>
2023-01-20 19:04:54 -0300

    refactor(header footer): refactor header and footer, and add colorTheme component to header

--- src/react/global/components/footer/footer.tsx
+++ src/react/global/components/footer/footer.tsx
@@ -1,13 +1,24 @@
 import './css/desktopFooter.css';
 import './css/portraitFooter.css';
 import './css/landscapeFooter.css';
+import { useSelector } from 'react-redux';
+import globalStrings from '../../constants/strings/globalStrings';
+import { useLanguage } from '../../../state-manager/redux/slices/sliceLanguage';
+import { useColorTheme } from '../../../state-manager/redux/slices/sliceColorTheme';
+import functions from '../../functions';
 
 function Footer() {
+  const [languageState] = useSelector(useLanguage);
+  const [themeState] = useSelector(useColorTheme);
+
+  const theme = functions.colorThemeSelector(themeState.colorTheme);
+  const translator = (text: any) => functions.languageSelector(languageState.toTranlate, text);
+
   return (
-    <footer id="footer">
-      <a href="https://github.com/ggiacomini2012" target="_blank" rel="noreferrer" data-testid="footer">
-        Developed by Guilherme Giacomini Teixeira
-        <img id="github-icon" alt="github Icon" src="../../../../assets/github-icon.png" />
+    <footer id="footer" className={`footer${theme}`}>
+      <a href="https://github.com/ggiacomini2012" target="_blank" rel="noreferrer" data-testid="footer" className={`footer-link${theme}`}>
+        © 2023 {translator(globalStrings.text.by)} {globalStrings.developerName}
+        <img id="github-icon" className={`github-icon${theme}`} alt="github Icon" src="../../../../assets/github-icon.png" />
       </a>
     </footer>
   );
---
+++ src/react/global/components/header/header.tsx
@@ -0,0 +1,47 @@
+import './css/desktopHeader.css';
+import './css/portraitHeader.css';
+import './css/landscapeHeader.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { changeLanguage, useLanguage } from '../../../state-manager/redux/slices/sliceLanguage';
+import {
+  useColorTheme,
+} from '../../../state-manager/redux/slices/sliceColorTheme';
+import functions from '../../functions';
+import ColorTheme from './components/colorThemeButton/colorTheme';
+
+function Header() {
+  const goTo = useNavigate();
+  const dispatch = useDispatch();
+  const [languageState] = useSelector(useLanguage);
+  const [themeState] = useSelector(useColorTheme);
+
+  const theme = functions.colorThemeSelector(themeState.colorTheme);
+  functions.languageSelector(languageState.toTranlate, '');
+
+  return (
+    <header id="header" className={`header${theme}`}>
+      <button className={`home-button${theme}`} type="button" onClick={() => goTo('/')}>
+        home
+      </button>
+      <button className={`about-button${theme}`} type="button" onClick={() => goTo('/about')}>
+        about
+      </button>
+      <button className={`redux-button${theme}`} type="button" onClick={() => goTo('/redux-playground')}>
+        redux
+      </button>
+      <select
+        name="select"
+        value={languageState.toTranlate}
+        className={`select-language${theme}`}
+        onChange={(e) => dispatch(changeLanguage(e.target.value))}
+      >
+        <option value="english">english</option>
+        <option value="portuguese">portuguese</option>
+      </select>
+      <ColorTheme />
+    </header>
+  );
+}
+
+export default Header;
