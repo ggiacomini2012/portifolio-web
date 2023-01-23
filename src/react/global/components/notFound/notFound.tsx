d15530241fe88ff361be3c4ac5d5171784fceab3
pro gui <ggiacomini2012@gmail.com>
2023-01-23 20:22:33 -0300

    feat(notfound.tsx): create notFound conponent and its css

---
+++ src/react/global/components/notFound/notFound.tsx
@@ -0,0 +1,35 @@
+import './css/desktopNotFound.css';
+import './css/portraitNotFound.css';
+import './css/landscapeNotFound.css';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import { useLanguage } from '../../../state-manager/redux/slices/sliceLanguage';
+import { useColorTheme } from '../../../state-manager/redux/slices/sliceColorTheme';
+import functions from '../../functions';
+import globalStrings from '../../constants/strings/globalStrings';
+
+function NotFound() {
+  const [languageState] = useSelector(useLanguage);
+  const [themeState] = useSelector(useColorTheme);
+
+  const theme = functions.colorThemeSelector(themeState.colorTheme);
+  const translator = (text: any) => functions.languageSelector(languageState.toTranlate, text);
+
+  useEffect(() => {
+    functions.fadeIn('not-found');
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <h1 id="not-found" data-testid="not-found" className={`not-found${theme} fade-in`}>
+        url not Found {translator(globalStrings.text.by)}
+      </h1>
+      <Footer />
+    </>
+  );
+}
+
+export default NotFound;
