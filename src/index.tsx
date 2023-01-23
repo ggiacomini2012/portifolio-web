6bd4f0268c3807e8f4d17e34bfadae5b6d92cbdf
pro gui <ggiacomini2012@gmail.com>
2023-01-23 20:20:20 -0300

    refactor(index.tsx): add BrowserRouter and remove BorwserRouter from app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import functions from './react/global/functions';
 
 const root = document.getElementById('root') as HTMLElement;
 
-functions.preLoad(ReactDOM, root, <App />);
+functions.preLoad(
+  ReactDOM,
+  root,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
