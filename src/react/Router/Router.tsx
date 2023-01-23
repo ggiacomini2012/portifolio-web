96c3418cd39f13ec64569061ad665140a39be717
pro gui <ggiacomini2012@gmail.com>
2023-01-23 20:21:19 -0300

    feat(router.tsx): add not-found route and component, change redux-playground route to contact

--- src/react/Router/Router.tsx
+++ src/react/Router/Router.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import functions from '../global/functions';
 import { useColorTheme } from '../state-manager/redux/slices/sliceColorTheme';
 import About from './pages/about/About';
 import Home from './pages/home/Home';
-import ReduxPlayground from './pages/reduxPlayground/ReduxPlayground';
+import Contact from './pages/contact/Contact';
 import './Router.css';
+import NotFound from '../global/components/notFound/notFound';
 
 function Router() {
   const [themeState] = useSelector(useColorTheme);
 
   const theme = functions.colorThemeSelector(themeState.colorTheme);
   return (
-    <div className={`router${theme}`}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/redux-playground" element={<ReduxPlayground />} />
-        </Routes>
-      </BrowserRouter>
+    <div id="router" className={`router${theme}`}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
