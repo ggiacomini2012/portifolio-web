dd9b8b8653989f44f7c0f3ca039a9558aac91782
pro gui <ggiacomini2012@gmail.com>
2023-01-25 07:31:45 -0300

    refactor(footer.tsx): add and refactor data-testid of all tags

--- src/react/global/components/footer/footer.tsx
+++ src/react/global/components/footer/footer.tsx
@@ -16,10 +16,10 @@ function Footer() {
   const translator = (text: any) => functions.languageSelector(languageState.toTranlate, text);
 
   return (
-    <footer id="footer" className={`footer${theme}`}>
-      <a href="https://github.com/ggiacomini2012" target="_blank" rel="noreferrer" data-testid="footer" className={`footer-link${theme}`}>
+    <footer id="footer" data-testid="footer" className={`footer${theme}`}>
+      <a href="https://github.com/ggiacomini2012" data-testid="footer-link" target="_blank" rel="noreferrer" className={`footer-link${theme}`}>
         © 2023 {translator(globalStrings.text.by)} {globalStrings.developerName}
-        <img id="github-icon" className={`github-icon${theme}`} alt="github Icon" src={githubIcon} />
+        <img id="github-icon" data-testid="footer-image" className={`github-icon${theme}`} alt="github Icon" src={githubIcon} />
       </a>
     </footer>
   );
