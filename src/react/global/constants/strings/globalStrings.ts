787605bb2ddf1de04b936b0bf6b1ed836a40c7a5
pro gui <ggiacomini2012@gmail.com>
2023-01-25 21:01:35 -0300

    feat(about|contact): add description to about and contact. centralize texts

--- src/react/global/constants/strings/globalStrings.ts
+++ src/react/global/constants/strings/globalStrings.ts
@@ -6,28 +6,32 @@ const globalStrings: Interface.IAllStrings = {
     by: 'by',
     home: 'home',
     about: 'about',
-    aboutDescription: 'about',
+    aboutDescription: 'aboutDescription',
     contact: 'contact',
-    contactDescription: 'contact',
+    contactDescription: 'contactDescription',
   },
   developerName: 'Guilherme Giacomini Teixeira.',
   english: {
-    intro: 'Hi, I\'m Guilherme Giacomini Teixeira, full stack web developer.',
+    intro: "Hi, I'm Guilherme Giacomini Teixeira, full stack web developer.",
     by: 'by',
     home: 'home',
     about: 'about',
-    aboutDescription: 'about description',
+    aboutDescription: `I’m a Front-End and Back-End Developer located in South of Brazil. Love UI effects.
+    I'm solutionist, Well-organised, with high attention to detail. Into making music, going for outdoor activities and socializing. Consider myself a continuos learner.
+    Speak fluently english, portuguese and spanish`,
     contact: 'contact',
-    contactDescription: 'contact description',
+    contactDescription: 'If you like what you see get in touch with me at:',
   },
   português: {
     intro: 'Olá, Guilherme Giacomini Teixeira, desenvolvedor web full stack.',
     by: 'por',
     home: 'início',
     about: 'sobre',
-    aboutDescription: 'sobre descrição',
+    aboutDescription: `Sou um desenvolvedor Front e Back. Moro em Balneário Camboriú - SC. 
+    Gosto muito de trabalhar com UI. Mantenho o foco em criar soluções, cultivo uma boa organização pessoal e presto muita atenção aos detalhes.
+    Crio músicas nas horas vagas, caminho, curto me reunir com amigos para tomar café e prosear. Tenho fluência no inglês e espanhol`,
     contact: 'contacto',
-    contactDescription: 'contacto descrição',
+    contactDescription: 'Se você gostou do meu trabalho me contacte através de algum destes canais:',
   },
   colorTheme: {
     darkMode: '-dark',
