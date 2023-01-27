f1c604e965630ccee696710c0964c156353174ec
pro gui <ggiacomini2012@gmail.com>
2023-01-27 12:05:27 -0300

    fix(globalstrings): fix breakline in descriptions
    it was making test the texts dificult

--- src/react/global/constants/strings/globalStrings.ts
+++ src/react/global/constants/strings/globalStrings.ts
@@ -16,9 +16,7 @@ const globalStrings: Interface.IAllStrings = {
     by: 'by',
     home: 'home',
     about: 'about',
-    aboutDescription: `I’m a Front-End and Back-End Developer located in South of Brazil. Love UI effects.
-    I'm solutionist, Well-organised, with high attention to detail. Into making music, going for outdoor activities and socializing. Consider myself a continuos learner.
-    Speak fluently english, portuguese and spanish`,
+    aboutDescription: 'I’m a Front-End and Back-End Developer located in South of Brazil. Love UI effects. I\'m solutionist, Well-organised, with high attention to detail. Into making music, going for outdoor activities and socializing. Consider myself a continuos learner. Speak fluently english, portuguese and spanish',
     contact: 'contact',
     contactDescription: 'If you like what you see get in touch with me at:',
   },
@@ -27,9 +25,7 @@ const globalStrings: Interface.IAllStrings = {
     by: 'por',
     home: 'início',
     about: 'sobre',
-    aboutDescription: `Sou um desenvolvedor Front e Back. Moro em Balneário Camboriú - SC. 
-    Gosto muito de trabalhar com UI. Mantenho o foco em criar soluções, cultivo uma boa organização pessoal e presto muita atenção aos detalhes.
-    Crio músicas nas horas vagas, caminho, curto me reunir com amigos para tomar café e prosear. Tenho fluência no inglês e espanhol`,
+    aboutDescription: 'Sou um desenvolvedor Front e Back. Moro em Balneário Camboriú - SC. Gosto muito de trabalhar com UI. Mantenho o foco em criar soluções, cultivo uma boa organização pessoal e presto muita atenção aos detalhes. Crio músicas nas horas vagas, caminho, curto me reunir com amigos para tomar café e prosear. Tenho fluência no inglês e espanhol',
     contact: 'contacto',
     contactDescription: 'Se você gostou do meu trabalho me contacte através de algum destes canais:',
   },
