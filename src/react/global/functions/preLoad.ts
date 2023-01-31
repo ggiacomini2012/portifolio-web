dd9e9460aa2f71566496ca8b846525a85ced875e
pro gui <ggiacomini2012@gmail.com>
2023-01-31 17:18:38 -0300

    test(preload): add test to preLoad function and separate settimeout as timer function

--- src/react/global/functions/preLoad.ts
+++ src/react/global/functions/preLoad.ts
@@ -1,12 +1,20 @@
-export default (react: any, root: HTMLElement, app: JSX.Element) => {
+const timer = (root: HTMLElement): void => {
   const rootExtractor = root;
-  react.createRoot(root).render(
-    app,
-  );
   setTimeout(() => {
-  rootExtractor!.style.display = 'flex';
+    rootExtractor!.style.display = 'flex';
   }, 300);
   setTimeout(() => {
-  rootExtractor!.style.opacity = '1';
+    rootExtractor!.style.opacity = '1';
   }, 1500);
+};
+
+export { timer };
+
+export default (react: any, root: HTMLElement, app: JSX.Element): HTMLElement => {
+  const react2 = react as unknown as any;
+  react2.createRoot(root).render(
+    app,
+  );
+  timer(root);
+  return root;
 };
---
+++ src/react/global/tests/preLoad.test.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-promise-executor-return */
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+// import { vi } from 'vitest';
+import { fixture, assert } from '@open-wc/testing';
+import App from '../../../App';
+import preLoad, { timer } from '../functions/preLoad';
+
+describe('Test createRoot', () => {
+  const root = document.createElement('div');
+  root.id = 'root';
+  root!.style.display = 'none';
+  root!.style.opacity = '0';
+
+  const result = preLoad(ReactDOM, root, <BrowserRouter><App /></BrowserRouter>);
+
+  it('Should return a HTML div with id "root":', () => {
+    expect(result).toBe(root);
+  });
+
+  it('Should "root" have display equal "none":', () => {
+    expect(root!.style.display).toBe('none');
+  });
+  it('Should "root" have opacity equal "0":', () => {
+    expect(root!.style.opacity).toBe('0');
+  });
+  it('Should "root" have opacity equal "1" when 1.5 seconds pass:', async () => {
+    await timer(root);
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 1500));
+    expect(root!.style.opacity).toBe('1');
+  });
+  it('Should "root" have display equal "flex" when 0.3 seconds pass:', async () => {
+    await timer(root);
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 300));
+    expect(root!.style.display).toBe('flex');
+  });
+
+  it('Should change display and opacity of root element after a time:', async () => {
+    const rootMock = await fixture('<div id="root" style="display: none; opacity: 0;"></div>') as HTMLElement;
+    timer(rootMock);
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 1500));
+    assert.equal(rootMock.style.display, 'flex');
+    assert.equal(rootMock.style.opacity, '1');
+  });
+});
