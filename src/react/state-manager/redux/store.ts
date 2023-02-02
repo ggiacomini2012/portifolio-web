7f6c2b292aa9bdf0f958092304a372ce9d8483d2
pro gui <ggiacomini2012@gmail.com>
2023-02-02 12:38:26 -0300

    refactor(store): remove unused slice

--- src/react/state-manager/redux/store.ts
+++ src/react/state-manager/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sliceColorTheme from './slices/sliceColorTheme';
 import sliceLanguage from './slices/sliceLanguage';
-import sliceLanguages from './slices/sliceLanguages';
 import sliceTemplate from './slices/sliceTemplate';
 
 const store = configureStore({
   reducer: {
-    languages: sliceLanguages,
     template: sliceTemplate,
     language: sliceLanguage,
     colorTheme: sliceColorTheme,
