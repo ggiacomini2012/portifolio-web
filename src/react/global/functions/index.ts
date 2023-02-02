c13e52cb57b8a8766428e3f60025ecea33582c05
pro gui <ggiacomini2012@gmail.com>
2023-02-02 12:53:35 -0300

    chore(functions/index): minor changes

--- src/react/global/functions/index.ts
+++ src/react/global/functions/index.ts
@@ -2,12 +2,10 @@ import colorThemeSelector from './colorThemeSelector';
 import fadeIn from './fadeIn';
 import languageSelector from './languageSelector';
 import preLoad from './preLoad';
-import translator from './translator';
 
 export default {
   colorThemeSelector,
   fadeIn,
   languageSelector,
   preLoad,
-  translator,
 };
