83314aa077ab1cba8e4e19a4a630838ae64d8378
pro gui <ggiacomini2012@gmail.com>
2023-02-04 11:21:58 -0300

    refactor(redux/store): delete slice template file and from store

--- src/react/state-manager/redux/store.ts
+++ src/react/state-manager/redux/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sliceColorTheme from './slices/sliceColorTheme';
 import sliceLanguage from './slices/sliceLanguage';
-import sliceTemplate from './slices/sliceTemplate';
 
 const store = configureStore({
   reducer: {
-    template: sliceTemplate,
     language: sliceLanguage,
     colorTheme: sliceColorTheme,
   },
